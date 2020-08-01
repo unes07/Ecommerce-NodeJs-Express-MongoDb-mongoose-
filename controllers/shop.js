cdb774ab4c69c6447d1a8e0e32b97ddfc5f1195e
YOUNES BOUDOUL <44946199+unes07@users.noreply.github.com>
2020-08-01 01:49:23 +0100

    Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,8 +44,9 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
-    .then((products) => {
-      console.log(products);
+    .execPopulate()
+    .then((user) => {
+      const products = user.cart.items;
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
@@ -70,7 +71,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then((result) => {
       res.redirect("/cart");
     })
